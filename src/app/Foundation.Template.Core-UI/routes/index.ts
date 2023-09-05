5b70834ce9918df034420245d5100fe2f771236f
qschroter <qschroter@gpi-experts.com>
2023-09-05 18:54:57 +0200

    fix route

--- src/app/Foundation.Template.Core-UI/routes/index.ts
+++ src/app/Foundation.Template.Core-UI/routes/index.ts
@@ -2,7 +2,7 @@ import { RouteRecordRaw } from "vue-router";
 
 export const routes: RouteRecordRaw[] = [
     {
-        path: "/organisations/:organisation-id/role-organisations/:roleId",
+        path: "/organisations/:organisationId/role-organisations/:roleId",
         name: "organisation-role-permissions",
         components: {
             default: () => import("../views/RoleOrganisation.vue")
