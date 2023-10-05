3af4d80d5d33ffd989b77ec2c0a10977fa25256b
gballigand <gballigand@dative-gpi.com>
2023-10-05 15:59:38 +0200

    add ui tests

--- src/app/Foundation.Template.Core-UI/index.ts
+++ src/app/Foundation.Template.Core-UI/index.ts
@@ -1,3 +1,5 @@
+import "./shims-vue.d.ts"
+
 export * from "./routes"
 export * from "./config"
 export * from "./domain"
---
+++ tests/app/Foundation.Template.Core-UI.Tests/tests/lib.test.ts
@@ -0,0 +1,8 @@
+/**
+ * @jest-environment jsdom
+ */
+import *  as all from '@dative-gpi/foundation-template-core-ui';
+
+test('basic', () => {
+  expect(all).toBeDefined();
+});
