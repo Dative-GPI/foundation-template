a149c52158be455ee840a63c34cb6ac4302bab2d
qschroter <qschroter@gpi-experts.com>
2023-10-12 11:58:28 +0200

    may fix issue with SSL error

--- dev/helpers/Foundation.Template.Proxy/Program.cs
+++ dev/helpers/Foundation.Template.Proxy/Program.cs
@@ -7,6 +7,10 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Foundation.Template.Proxy.Tools;
+using System.Net;
+using Yarp.ReverseProxy.Forwarder;
+using System.Diagnostics;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -37,6 +41,21 @@ app.UseForwardedHeaders();
 app.UseRouting();
 
 app.MapControllers();
-app.MapForwarder("/{**catch-all}", app.Configuration.GetConnectionString("Foundation"));
+
+var httpClient = new HttpMessageInvoker(new SocketsHttpHandler()
+{
+    UseProxy = false,
+    AllowAutoRedirect = false,
+    AutomaticDecompression = DecompressionMethods.None,
+    UseCookies = false,
+    ActivityHeadersPropagator = new ReverseProxyPropagator(DistributedContextPropagator.Current),
+    ConnectTimeout = TimeSpan.FromSeconds(15),
+    SslOptions = new System.Net.Security.SslClientAuthenticationOptions()
+    {
+        RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+    }
+});
+
+app.MapForwarder("/{**catch-all}", app.Configuration.GetConnectionString("Foundation"), ForwarderRequestConfig.Empty, HttpTransformer.Default, httpClient);
 
 app.Run();
