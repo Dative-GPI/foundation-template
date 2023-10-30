2b00cfd7524fad3a4506bc8017074e41909f3a8e
qschroter <qschroter@gpi-experts.com>
2023-10-30 13:53:29 +0100

    may fix issue with organisationId

--- src/app/Foundation.Template.Core-UI/composables/useOrganisationId.ts
+++ src/app/Foundation.Template.Core-UI/composables/useOrganisationId.ts
@@ -1,16 +1,25 @@
-import { ref, watch } from "vue";
+import { onMounted, onUnmounted, provide, ref, watch } from "vue";
 import { useRouter } from "vue-router";
+import { ORGANISATION_ID } from "../config/literals";
+
+let initiliazed = false;
+const organisationId = ref<string | null>(null);
 
 export const useOrganisationId = () => {
 
+    if (initiliazed) return {
+        organisationId
+    }
+
+    initiliazed = true
+
     const router = useRouter();
-    const organisationId = ref<string | null>(null);
 
     watch(router.currentRoute, () => {
-        console.log("current route changed");
         organisationId.value = router.currentRoute.value.params.organisationId as string | null;
+        provide(ORGANISATION_ID, organisationId);
     })
-
+    
     return {
         organisationId
     }
--- src/app/Foundation.Template.Core-UI/config/urls/urlFactory.ts
+++ src/app/Foundation.Template.Core-UI/config/urls/urlFactory.ts
@@ -1,9 +1,29 @@
-import { useOrganisationId } from "../../composables/useOrganisationId";
+import { Ref, inject, ref } from "vue";
+import { useRoute } from "vue-router";
+
+import { ORGANISATION_ID } from "../literals";
 
 export function urlFactory(url: (organisationId: string) => string) {
     return () => {
-        const { organisationId } = useOrganisationId();
-        if (!organisationId.value) throw new Error("Organisation ID is not defined");
-        return url(organisationId.value);
+        const route = useRoute();
+
+        let result: string | null = null;
+
+        let organisationId: Ref<string | null> = inject(ORGANISATION_ID, ref<string | null>(null));
+        
+        result = organisationId.value;
+
+        if (!result) {
+            if ((!route || !route.params)) {
+                console.warn("You are trying to get organisationId without providing it nor in your route params. Please add it.", route, route.params)
+            }
+            else {
+                result = route.params[ORGANISATION_ID] as string;
+            }
+        }
+
+        if (!organisationId) throw new Error("Organisation ID is not defined");
+
+        return url(result);
     }
 }
