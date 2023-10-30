2172e8d65c555ae2218cd44b96f698793095c879
qschroter <qschroter@gpi-experts.com>
2023-10-30 13:54:25 +0100

    refacto

--- src/app/Foundation.Template.Core-UI/config/urls/urlFactory.ts
+++ src/app/Foundation.Template.Core-UI/config/urls/urlFactory.ts
@@ -5,15 +5,15 @@ import { ORGANISATION_ID } from "../literals";
 
 export function urlFactory(url: (organisationId: string) => string) {
     return () => {
-        const route = useRoute();
-
         let result: string | null = null;
 
         let organisationId: Ref<string | null> = inject(ORGANISATION_ID, ref<string | null>(null));
-        
+
         result = organisationId.value;
 
         if (!result) {
+            const route = useRoute();
+
             if ((!route || !route.params)) {
                 console.warn("You are trying to get organisationId without providing it nor in your route params. Please add it.", route, route.params)
             }
