2b6bd42c0072df0a560734ab2a206427c86d1274
ddonfack <ddonfack@dative-gpi.com>
2024-03-06 12:07:28 +0100

    unused files

--- src/app/Foundation.Template.Core-UI/config/urls/tables.ts
+++
@@ -1,5 +0,0 @@
-import { BASE_URL } from "./base";
-
-export const TABLES_URL = `${BASE_URL}/tables`;
-export const TABLE_URL = (id: string) => `${TABLES_URL}/${encodeURIComponent(id)}`
-export const TABLE_PROPERTIES_URL = (id: string) => `${TABLE_URL(id)}/properties`
--- src/app/Foundation.Template.Core/ViewModels/Dispositions/OrganisationTypeTableDetailsViewModel.cs
+++
@@ -1,6 +0,0 @@
-namespace Foundation.Template.Core.ViewModels
-{
-    public class OrganisationTypeTableDetailsViewModel : OrganisationTypeTableInfosViewModel
-    {
-    }
-}
