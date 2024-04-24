a3d951c62de9cbb829a8025bdd28a963097c0f9f
peyronth <peyron.th@gmail.com>
2024-04-24 11:33:22 +0200

    Fix setAppTimeZone to get machine app time zone or user app Time zone if set.

--- src/app/Foundation.Template.Core-UI/composables/useCoreTemplate.ts
+++ src/app/Foundation.Template.Core-UI/composables/useCoreTemplate.ts
@@ -1,5 +1,6 @@
 import { onMounted, ref } from "vue";
 import { useExtensionHost, useTranslationsProvider } from "@dative-gpi/foundation-template-shared-ui";
+import { useAppTimeZone, useCurrentUser } from "@dative-gpi/foundation-shared-services/composables"
 
 import { useOrganisationId } from "./useOrganisationId";
 import { usePermissionsProvider } from "./usePermissionsProvider";
@@ -18,11 +19,26 @@ export function useCoreTemplate() {
 
     const { init: initPermissions } = usePermissionsProvider();
     const { init: initTranslations } = useTranslationsProvider();
+    const { fetch: fetchCurrentUser } = useCurrentUser()
+    
+    const { setAppTimeZone, getMachineOffset } = useAppTimeZone();
 
     onMounted(async () => {
         await initialized
         await initTranslations();
         await initPermissions();
+        setAppTimeZone({
+            id: Intl.DateTimeFormat().resolvedOptions().timeZone,
+            offset: getMachineOffset()
+        });
+
+        const user = await fetchCurrentUser()
+        if (user.value.timeZoneId && user.value.timeZoneOffset) {
+            setAppTimeZone({
+                id: user.value.timeZoneId,
+                offset: user.value.timeZoneOffset
+            });
+        }
 
         ready.value = true;
     });
